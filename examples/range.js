a182033fc59ce06574180ae5faed1a44f75077f1
Sonny Piers <sonny@fastmail.net>
2017-09-20 14:54:20 +0200

    set example range to 3

--- examples/range.js
+++ examples/range.js
@@ -4,7 +4,7 @@ const m = require("..");
 
 m.once("movuino", movuino => {
   movuino.once("plugged", async () => {
-    await movuino.setRange({ accel: 2, gyro: 2 });
+    await movuino.setRange({ accel: 3, gyro: 3 });
     const range = await movuino.getRange();
     console.log(range);
   });
